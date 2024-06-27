41472fe92b0f80b7ed240340257da7eb3ca8bd1e
m-ogore <ogoremarv@gmail.com>
2024-06-27 09:57:06 +0200

    Updated: requests.head - modularized - move to next page

--- link_valid.py
+++ link_valid.py
@@ -1,15 +1,27 @@
 import scraper
 import requests
-
+from tqdm import tqdm
 
 def link_valid(dataframe, status_code):
-    for url in dataframe['URL']:
-        response = requests.get(url)
-
-        status_code.append(response.status_code)
+    for url in tqdm(dataframe['URL'], desc="Processing URLs", unit="url"):
+        if url.startswith('s3://'):
+            print(f"Skipping S3 URL: {url}")
+            status_code.append(None)  # Append None for S3 URLs
+            continue
+        
+        try:
+            response = requests.head(url, timeout=5)  # Set a timeout of 5 seconds
+            status_code.append(response.status_code)
+        except requests.exceptions.InvalidSchema:
+            print(f"Invalid schema found for URL: {url}")
+            status_code.append(None)  # Append None to indicate the error
+        except requests.exceptions.RequestException as e:
+            # Catch other possible request exceptions, including timeouts
+            print(f"Error fetching {url}: {e}")
+            status_code.append(None)  # Append None to indicate the error
 
     dataframe['status_code'] = status_code
-
+    dataframe.to_csv('aluswe.csv', index=False)
 
 if __name__ == '__main__':
     dataframe = scraper.scraper()
--- scraper.py
+++ scraper.py
@@ -3,8 +3,10 @@ from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import pandas as pd
 # from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-def scraper():
+def get_pages():
 
     PASSWORD = open('creds.txt', 'r').read()
 
@@ -26,34 +28,62 @@ def scraper():
     password_field.send_keys(PASSWORD)
     login_btn.click()
 
-    page_source = my_driver.page_source
-    my_driver.close()
-
-    my_soup = BeautifulSoup(page_source, 'html.parser')
-
-    my_table = my_soup.find('table', {'class': 'table table-striped sortable'})
-
-    my_headers = [header.text for header in my_table.find('tr').find_all('th')]
-
-    my_headers.extend(['',''])
-
-    my_df = pd.DataFrame(columns=my_headers if my_headers else None)
+    page_sources = []
+    
+    try:
+        while True:
+            # Assuming 'commit' is the name of the next button you want to click
+            next_btn = WebDriverWait(my_driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='universal-pagination']/a[contains(text(), 'Next')]")))
+            
+            # Append the current page source to the list
+            page_sources.append(my_driver.page_source)
+            
+            # Click the next button
+            next_btn.click()
+            
+            # Optionally, you can add a delay here if necessary
+            WebDriverWait(my_driver, 10).until(lambda driver: driver.execute_script('return document.readyState') == 'complete')
+    
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    
+    finally:
+        my_driver.quit()
+    print(f"{'#'*5} Done getting page sources {'#'*5}")
+    return page_sources
+def scraper():
 
     my_data = []
+
+    page_sources = get_pages()
+    for page_source in page_sources:
+        my_soup = BeautifulSoup(page_source, 'html.parser')
+
+        my_table = my_soup.find('table', {'class': 'table table-striped sortable'})
+
+        my_headers = [header.text for header in my_table.find('tr').find_all('th')]
+
+        my_headers.extend(['',''])
+
+        my_df = pd.DataFrame(columns=my_headers if my_headers else None)
+
     
-    for row in my_table.find('tbody').find_all('tr'):
-        cells = row.find_all('td')
+    
+        for row in my_table.find('tbody').find_all('tr'):
+            cells = row.find_all('td')
 
-        ROW = [cell.text.strip() for cell in cells]
+            ROW = [cell.text.strip() for cell in cells]
 
-        my_data.append(ROW)
+            my_data.append(ROW)
 
     print("Data:", len(my_data[0]))
     print(my_data[0])
     print("headers:", len(my_headers))
 
     my_df = pd.DataFrame(my_data, columns=my_headers if my_headers else None)
-    my_df.to_csv('aluswe.csv', index=False)
+    #my_df.to_csv('aluswe.csv', index=False)
+
+    print(f"{'#'*5} Done putting in dataframe {'#'*5}")
 
 
     return my_df
